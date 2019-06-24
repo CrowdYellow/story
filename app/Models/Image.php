bec76a737c71badc17ce6e07cefd8ab3e283ddf1
CrowdYellow <echo61546@gmail.com>
2019-06-24 19:13:23 +0800

    图片路径

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -8,13 +8,4 @@ use Illuminate\Support\Str;
 class Image extends Model
 {
     public $guarded = [];
-
-    public function getImageUrlAttribute()
-    {
-        // 如果 image 字段本身就已经是完整的 url 就直接返回
-        if (Str::startsWith($this->attributes['images'], ['http://', 'https://'])) {
-            return $this->attributes['images'];
-        }
-        return \Storage::disk('public')->url($this->attributes['images']);
-    }
 }
--- app/Models/System.php
+++ app/Models/System.php
@@ -8,13 +8,4 @@ use Illuminate\Support\Str;
 class System extends Model
 {
     public $guarded = [];
-
-    public function getImageUrlAttribute()
-    {
-        // 如果 image 字段本身就已经是完整的 url 就直接返回
-        if (Str::startsWith($this->attributes['we_chat'], ['http://', 'https://'])) {
-            return $this->attributes['we_chat'];
-        }
-        return \Storage::disk('public')->url($this->attributes['we_chat']);
-    }
 }
